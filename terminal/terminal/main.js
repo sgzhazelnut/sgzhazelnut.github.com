c2ac1cda6c028caca8e1ca059b438bcaa4373388
Lance She <15201752137@163.com>
2019-03-07 07:32:09 +0800

    Add the GitHub support

--- terminal/terminal/main.js
+++ terminal/terminal/main.js
@@ -103,7 +103,7 @@ var files = (function () {
     Singleton.defaultOptions = {};
     Singleton.defaultOptions["github.txt"] = "https://github.com/hazelnutsgz";
     Singleton.defaultOptions["linkedin.txt"] = "https://www.linkedin.com/in/hazelnutsgz/"
-    Singleton.defaultOptions["resume.pdf"] = "In the pdf format, please check https://sgzhazelnut.github.io/CV/GuozhenShe.pdf"
+    Singleton.defaultOptions["resume.pdf"] = "https://sgzhazelnut.github.io/CV/GuozhenShe.pdf"
     Singleton.defaultOptions[(isUserPT ? "sobre.txt" : "interests.txt")] = "Archeology (on Computer Science), Soccer(DM), Road Cycling(ITT)";
     Singleton.defaultOptions[(isUserPT ? "LLL" : "courses.txt")] = "\
     Fudan University, Shanghai, China\t\t\t2015.9-Present\n\
