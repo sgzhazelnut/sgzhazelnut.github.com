63ec0342b753f3d104b27005b664ec6addac761c
hazelnutsgz <15201752137@163.com>
2019-08-27 13:44:47 -0500

    Reorder

--- terminal/terminal/main.js
+++ terminal/terminal/main.js
@@ -34,29 +34,7 @@ var configs = (function () {
         rmdir_help: "Remove directory, this command will only work if the folders are empty.",
         touch_help: "Change file timestamps. If the file doesn't exist, it's created an empty one.",
         sudo_help: "Execute a command as the superuser.",
-        welcome: "::::::;;:::;;;;;;;;;;;;;;;;;;;;;;;::::::\n\
-        ;;;;;;;;,,'''''''''''''''''''',,;;;:::::\n\
-        ;;;;;,,''''''''''''''''''''''''',,;;;:::\n\
-        ;;;;,,'''''''''''''',,'',,,,,,,,,,,,;;::\n\
-        ;;;,,'.'',,,;:cc:ccccccc:::;;;,,,,,,;;::\n\
-        ;;,,'.',;:clloddddxxxxxxddoollc;,,,,;;::\n\
-        ;;;,'',:lloddxxxxxkkkkkkxxxxxxdl;,,,,;::\n\
-        ;;;,'';clooddxxxxkkkkkkkkkxxxxxoc;,,;;;:\n\
-        ;;;,'';cloooooddxxxxxxxxxdxxxxxdl:;,;;::\n\
-        ;;,,',:llllooooooddddddodddddxxxdl;,;:::\n\
-        ;,;,,;clllllllllooddddolllooddxxxo::clc:\n\
-        ,,;;;;:clllcccllloooooolccloddxxxoclddl:\n\
-        ,,,;;;:coooooooooodddddddddxxxkkxdoodoc:\n\
-        ,,,;;::cloddddddddxxdddxxxxkkkkkkxxddlc:\n\
-        ,,,,;:cllodxxxxdooddooodxkkkkkkkkkxdl:::\n\
-        ,,,,,;:clodddxddolooooddxkkkxxkkxolc::::\n\
-        ,,,,,,,;clooodddddddddxxxxxxxxxdl::;::::\n\
-        ,,,,,,,,;cloooolllooooooddxxxdolc:::::;:\n\
-        ,,,,,,,,,;:cooooooodddddddxddol:::::::;:\n\
-        ,,,,,,,,,,;:cloooddxxxxxxddoolc;:lc::;;:\n\
-        ,,,,,,,,;:cc::cclooodddoooolllc::looc:::\n\
-        ,,,,,,,;codo:;:ccccllllllloollc::ldxdoc:\n\n\n\n\
-        Welcome to hazelnutsgz's website! :)\nHis name is Guozhen She, yet another CS guy from Fudan university supervised by Prof Yang Chen.\n\n\
+        welcome: "Welcome to hazelnutsgz's website! :)\nHis name is Guozhen She, yet another CS guy from Fudan university supervised by Prof Yang Chen.\n\n\
         Career-wise, She is an apprentice at System and Network Group in Microsoft Research Asia. Before that, She struggled with OpenStack at Intel Open Source Technology Center. Long long ago, She served as a (so-called) full-stack engineer at an e-commercial startup called ContextLogic(Wish)\n...........\n\n\n\
         As a 'student', She is equipped with neither decent GPA(3.55, 17.9%) nor fruitful publications (zero first-author pub), and even suspended from school for one semester because of the surgery.\n\n\
         She's interest resides in the intersection(periphery) of HCI, Distributed System(in a naive manner), Security(system-wise), PL(out of curiosity), Magic(machine learning etc.)\n\n\
